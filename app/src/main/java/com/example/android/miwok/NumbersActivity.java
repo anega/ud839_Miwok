89ba01ec6423dc01f16bc44298ac31019e751d97
anega <anegamega@gmail.com>
2017-08-27 17:28:34 +0300

    Add Numbers list of words as array

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+        int i = 0;
+        for (String word:
+             words) {
+            Log.v("Jopka", "Word at index " + (i++) + ": " + word);
+        }
     }
 }
